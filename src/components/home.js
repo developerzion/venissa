60694a2dc99e90065bbd5dbaaed6f9cfab4f6e5c
Moses Samuel <devparse.official@gmail.com>
2021-12-17 12:11:35 +0100

    Added container section

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../images/logo.png"
+import banner from "../images/banner.jpg"
 
 const Home =()=>{
 
@@ -33,12 +34,29 @@ const Home =()=>{
                 <div className="mobilenav">
                     <span onClick={ (e)=>{ e.target.classList.toggle("fa-times"); document.querySelector(".navbar").classList.toggle("active")} } className="fa fa-bars"></span>
                     <span className="fa fa-user"></span>
-                </div>           
-
+                </div>  
 
                 {/* End div menu  */}
 
             </div>
+
+            <section id="banner" className="banner">
+
+                <div className="container bd-grid">
+                    <div className="container-left">
+                        <h1>The Design Thinking superpowers</h1>
+                        <h4>Tools, tutorials, design and innovation experts, all in one place! The most intuitive way to imagine your next user experience.</h4>
+                        <div className="container-footer">
+                            <Link to="" className="custom-button">Get Started</Link>
+                            <Link to="" className="custom-text"> <i className="fa fa-play"></i> Watch the video</Link>
+                        </div>
+                    </div>
+                    <div className="container-right">
+                        <img src={banner} alt="Banenr" />
+                    </div>
+                </div>
+
+            </section>
 
       
         </div>
