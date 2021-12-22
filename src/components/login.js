a6a0e1588a623a92be730fd267ebf716895a15fd
Moses Samuel <devparse.official@gmail.com>
2021-12-22 08:56:51 +0100

    Added login section

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Header from "./header"
+// import Header from "./header"
 
 import user from '../images/user.png'
 import password from '../images/lock.png'
@@ -10,46 +10,47 @@ import img from '../images/log.svg';
 const Login =()=>{
     return(
         <div>
-            <Header/>
+            <section className="section">
+                {/* <Header/> */}
             
 
-            <div className="container-login">
+                <div className="container-login">           
 
-           
+                    <div className="login">
 
-                <div className="login">
-                    
-                    <div className="log-header">                    
-                        <img src={img} alt="Log"/>
+                        <div className="log-header">                    
+                            <img src={img} alt="Log"/>
+                        </div>
+
+                        <div className="login-text">
+                            <h1>Welcome Back</h1>
+                            <h3>Login to your existing account</h3>
+                        </div>
+
+                        <form className="login-form">
+
+                            <div className="login-field">
+                                <img src={user} alt="user" />
+                                <input type="text" placeholder="Enter your username" required=""/>
+                            </div>
+                            <div className="login-field">
+                                <img src={password} alt="password" />
+                                <input type="password" placeholder="Password" required="" />
+                            </div>
+
+                            <div className="forgot-password">
+                                <Link to="">Forgot Password?</Link> 
+                            </div>
+
+                            <button className="custom-button center padTop">Sign In</button>
+
+                            <p className="signUp center">Don’t have an account?&nbsp;<Link to=""> Sign Up</Link></p>
+
+                        </form>
                     </div>
-
-                    <div className="login-text">
-                        <h1>Welcome Back</h1>
-                        <h3>Login to your existing account</h3>
-                    </div>
-
-                    <form className="login-form">
-
-                        <div className="login-field">
-                            <img src={user} alt="user" />
-                            <input type="text" placeholder="Enter your username" required=""/>
-                        </div>
-                        <div className="login-field">
-                            <img src={password} alt="password" />
-                            <input type="password" placeholder="Password" required="" />
-                        </div>
-
-                        <div className="forgot-password">
-                            <Link to="">Forgot Password?</Link> 
-                        </div>
-
-                        <button className="custom-button center padTop">Sign In</button>
-
-                        <p className="signUp center">Don’t have an account?&nbsp;<Link to=""> Sign Up</Link></p>
-
-                    </form>
                 </div>
-            </div>
+            </section>
+            
         </div>
     )
 }
