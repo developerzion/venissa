b298783d3632eef50c2313785cc53c8cb11b608b
Moses Samuel <devparse.official@gmail.com>
2022-01-21 21:06:23 +0100

    new commit

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -7,35 +7,39 @@ import banner from './images/banner.png'
 
 const Body =()=>{
     return (
-        <div className="mainBody">
-            <div className="bodyContainer">
-                <div className="boxText">
-                    <h5>Hi There,</h5>
-                    <h1>I am Ven<span>i</span>ssa</h1>
-                    <h2>I am a designer during daytime</h2>
-                    <h3>& work with batman during nights.</h3>
-                    <Link to="/">Ask Me How</Link>
+        <div>
+            {/* <div className="circle"></div> */}
 
-                    <div className="contact">
-                        <div className="box">
-                            <h3>Email</h3>
-                            <p>devparse.official@gmail.com</p>
-                        </div>
-                        <div className="box">
-                            <h3>Phone</h3>
-                            <p>+234 703-619-5368</p>
-                        </div>
-                        <div className="box">
-                            <h3>Location</h3>
-                            <p>Lagos state, Nigeria.</p>
+            <div className="mainBody">
+                <div className="bodyContainer">
+                    <div className="boxText">
+                        <h5>Hi There,</h5>
+                        <h1>I am Ven<span>i</span>ssa</h1>
+                        <h2>I am a designer during daytime</h2>
+                        <h3>& work with batman during nights.</h3>
+                        <Link to="/">Ask Me How</Link>
+
+                        <div className="contact">
+                            <div className="box">
+                                <h3>Email</h3>
+                                <p>devparse.official@gmail.com</p>
+                            </div>
+                            <div className="box">
+                                <h3>Phone</h3>
+                                <p>+234 703-619-5368</p>
+                            </div>
+                            <div className="box">
+                                <h3>Location</h3>
+                                <p>Lagos state, Nigeria.</p>
+                            </div>
                         </div>
                     </div>
+                    <div className="boxImage">
+                        <img src={banner} alt="banner" />
+                    </div>
                 </div>
-                <div className="boxImage">
-                    <img src={banner} alt="banner" />
-                </div>
+                
             </div>
-            
         </div>
         
     )
